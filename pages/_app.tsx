1b4396cbf35ef73842462ecd97ef06c09df07372
Vikas Satpute <vikas.s.satpute.com>
2022-07-03 00:09:32 +0530

    project initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useState } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+    setIsSSR(false)
+  }, [])
+
+  if (isSSR) return null;
+
+
+  return (
+    <div>
+      Navbar
+      <div className="flex gap-6 xl:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:overflow-auto">
+          Sidebar
+        </div>
+        <div className="mt-4 flex flex-col ga-10 overflow-auto h-[88vh] videos flex-1"></div>
+      </div>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
